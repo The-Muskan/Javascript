ee0c3c55f7bbc1254a10a0b4dd14dc5e4b47e20b
Muskan Chourasia <chourasiamuskan830@gmail.com>
2024-04-13 14:30:34 +0000

    ..

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict";   // Treat all js code as newer version
+// alert("Hii")     We are using nodejs, not browser. 
+
+// Different DataTypes :-
+// number, bigint, string, boolean, symbol, object, null, undefined...
+let val =" hiii";  
+console.log(val);
+
+
+
+
